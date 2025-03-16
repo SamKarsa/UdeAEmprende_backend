7a0b1cc4923b759d029f26e1d2bd6054dd2d9b92
SamKarsa <samuellopezmarin2006@gmail.com>
2025-03-16 15:51:36 -0500

    first commit

---
+++ src/main/java/com/emprendimientos/udea_emprende/UdeaEmprendeApplication.java
@@ -0,0 +1,13 @@
+package com.emprendimientos.udea_emprende;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdeaEmprendeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdeaEmprendeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emprendimientos/udea_emprende/UdeaEmprendeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emprendimientos.udea_emprende;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdeaEmprendeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
