a375df0a7aaaa03f7419055e43f7ed2a62a7e3aa
Alexander Osorio <alextrespalaciososorio@gmail.com>
2025-03-30 15:23:51 -0500

    Add the crossorigin notation to all controllers to allow the comunication with angular

--- src/main/java/com/emprendimientos/udea_emprende/controller/UserController.java
+++ src/main/java/com/emprendimientos/udea_emprende/controller/UserController.java
@@ -9,6 +9,7 @@ import com.emprendimientos.udea_emprende.service.UserService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class UserController {
 
     @Autowired
